abb8d0736cd891d7644c51ec1c3ef3ca47b369e3
Meena <meenaarumugam@gmail.com>
2018-12-08 04:42:20 -0600

    Finished  program

---
+++ src/excercises/Array1.java
@@ -0,0 +1,13 @@
+package excercises;
+import java.util.Arrays;
+
+public class Array1 {
+    public static void main(String[] args) {
+        int[] narray = {1, 1, 2, 3, 5, 8};
+
+        for (int i = 0; i < narray.length; i++){
+            System.out.println(narray[i]);
+        }
+    }
+
+}
